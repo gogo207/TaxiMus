575c98ab941ec7117bedbf047bfba6318d375bbb
mustafa265 <mustafave@code.edu.az>
2018-09-17 17:08:03 +0400

    17

---
+++ TaxiMus/TaxiMus/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaxiMus.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Taxi_AppEntities TMDB = new Taxi_AppEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
