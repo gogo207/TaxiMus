a50734e1829a17c1199ced4333b8a6a920dc5732
hesenovzamin <hesenovzamin@gmail.com>
2018-09-21 15:29:06 +0400

    in case of error

--- TaxiMus/TaxiMus/Controllers/VehicleController.cs
+++ TaxiMus/TaxiMus/Controllers/VehicleController.cs
@@ -16,31 +16,59 @@ namespace TaxiMus.Controllers
         [HttpGet]
         public ActionResult Create()
         {
+            var manufacturerlist = (from manufacturer in TMDB.Manufacturers.ToList()
+                                   select manufacturer.Name).ToList();
+            ViewBag.Manufacturerlist = manufacturerlist;
+
+            var modellist = (from model in TMDB.Models.ToList()
+                                    select model.Name).ToList();
+            ViewBag.Modellist = modellist;
+
+            var categorylist = (from category in TMDB.Categories.ToList()
+                                    select category.Name).ToList();
+            ViewBag.Categorylist = categorylist;
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(string platenumber, int manufacturerid, int modelid, int categoryid, int year)
+        public ActionResult Create(string platenumber, string manufacturer, string model, string category, int year)
         {
-            var manufacturerids = (from manufacturer in TMDB.Models.ToList()
-                                   select manufacturer.ID).ToList();
+            //var manufacturerids = (from manufacturer in TMDB.Manufacturers.ToList()
+            //                       select manufacturer.ID).ToList();
+
+            var manid = (from manuid in TMDB.Manufacturers
+                        where manufacturer == manuid.Name
+                        select manuid.ID).ToList();
+
+            var modid = (from modelid in TMDB.Models
+                         where model == modelid.Name
+                         select modelid.ID).ToList();
+
+            var catid = (from cateid in TMDB.Categories
+                         where category == cateid.Name
+                         select cateid.ID).ToList();
+
+
+
+
+
 
             Vehicle newVehicle = new Vehicle();
             newVehicle.PlateNumber = platenumber;
-            newVehicle.ManufacturerID = manufacturerid;
-            newVehicle.ModelID = modelid;
-            newVehicle.CategoryID = categoryid;
+            newVehicle.ManufacturerID = manid[0];
+            newVehicle.ModelID = modid[0];
+            newVehicle.CategoryID = catid[0];
             newVehicle.Year = year;
             TMDB.Vehicles.Add(newVehicle);
             TMDB.SaveChanges();
-            ViewBag.ManufacturerIDs = manufacturerids;
+            //ViewBag.ManufacturerIDs = manufacturerids;
             return RedirectToAction("Read");
         }
 
         public ActionResult Read()
         {
-            ViewBag.Vehicles = TMDB.Vehicles.ToList();
-            return View();
+            //ViewBag.Vehicles = 
+            return View(TMDB.Vehicles.ToList());
         }
     }
 }
